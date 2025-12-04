10d23cdf8f3d87a138d3510e7ed2da9c695b8158
BRAYAN E. PAREDES CABRERA <117842524+brenpc1443@users.noreply.github.com>
2025-12-04 06:34:20 -0500

    Update metricas.js

--- routes/metricas.js
+++ routes/metricas.js
@@ -4,14 +4,33 @@ const {
   obtenerMetricas,
   obtenerMetricasPorTurno,
 } = require("../controllers/metricasController");
+const {
+  obtenerHistorial,
+  obtenerReporte,
+} = require("../controllers/sesionesController");
 const { verificarAuth, verificarRol } = require("../middleware/auth");
 
 router.get("/", verificarAuth, verificarRol("administrador"), obtenerMetricas);
+
 router.get(
   "/turno",
   verificarAuth,
   verificarRol("administrador"),
   obtenerMetricasPorTurno
+);
+
+router.get(
+  "/historial",
+  verificarAuth,
+  verificarRol("administrador"),
+  obtenerHistorial
+);
+
+router.get(
+  "/reporte",
+  verificarAuth,
+  verificarRol("administrador"),
+  obtenerReporte
 );
 
 module.exports = router;
